9e9d92e9170c2ea553f9fb81da4f6bb909eb53a5
raultolive <yfert_0209@hotmail.com>
2019-05-13 19:26:48 -0400

    Update AppPage.java

--- src/main/java/pivotal/ui/pages/AppPage.java
+++ src/main/java/pivotal/ui/pages/AppPage.java
@@ -4,6 +4,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
+/**
+ * AppPage class
+ *
+ * @author raulchoque
+ * @version 0.0.1
+ */
 public class AppPage extends BasePage {
 
     @FindBy(css = "header.header__container")
